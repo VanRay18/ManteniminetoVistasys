925e7134012af13135ec4cdf9c918336b299337b
VanRay18 <150478500+VanRay18@users.noreply.github.com>
2024-04-25 08:32:59 -0600

    Agrege comentarios

--- ProyectoEstancia/Site.Master.cs
+++ ProyectoEstancia/Site.Master.cs
@@ -13,7 +13,7 @@ namespace ProyectoEstancia
     public partial class SiteMaster : MasterPage
     {
         private const string AntiXsrfTokenKey = "__AntiXsrfToken";
-        private const string AntiXsrfUserNameKey = "__AntiXsrfUserName";
+        private const string AntiXsrfUserNameKey = "__AntiXsrfUserName"; //coemtairos
         private string _antiXsrfTokenValue;
 
         protected void Page_Init(object sender, EventArgs e)
