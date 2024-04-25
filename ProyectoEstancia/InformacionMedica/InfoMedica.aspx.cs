adf419f88770eb68021f6c60d681cf447c618633
iroonmooonguer <128271762+iroonmooonguer@users.noreply.github.com>
2024-04-25 12:55:22 -0600

    modificación y conexión a la bd de el archivo  de infoMedica
    modificacion de backend del archivo InfoMedica.cs con estoes cambios se brinda un mantenimiento mas adecuado y seguro para las consultas de cada peticion a la base de datos
    tambien se agrego la conexion a la base de datos conexion a base de datos

--- ProyectoEstancia/InformacionMedica/InfoMedica.aspx.cs
+++ ProyectoEstancia/InformacionMedica/InfoMedica.aspx.cs
@@ -16,7 +16,7 @@ namespace ProyectoEstancia.InformacionMedica
         }
         public string Get_ConectionString()
         {
-            string SQLServer_Conection_String = "Data Source=ROSE\\SQLEXPRESS; Initial Catalog=bddatos; User ID= sa; Password=aaa";
+            string SQLServer_Conection_String = "Data Source=MSI\SQLEXPRESS;Initial Catalog=VITASYS;Integrated Security=True";
             return SQLServer_Conection_String;
         }
         public void BorrarTxt()
@@ -30,29 +30,35 @@ namespace ProyectoEstancia.InformacionMedica
            
         }
 
-     
+
 
         protected void btnInsertar_Click1(object sender, EventArgs e)
         {
             try
             {
-                string sql;
-                SqlCommand mycmd = new SqlCommand();
-                SqlDataReader reader;
-                SqlConnection conexion = new SqlConnection(Get_ConectionString());
-                conexion.Open();
-                sql = "INSERT INTO [dbo].[InformacionMedica] ([NSS],[Nombre],[Edad],[TipoSangre],[Enfermedad],[Medicamentos]) VALUES('" + txtNSS.Text + " ','" + txtNombre.Text + " ','" + txtEdad.Text + "','" + txtTipoSangre.Text + "','" + txtEnfermedad.Text + "','" + txtMedicamentos.Text + "')";
-                mycmd.CommandText = sql;
-                mycmd.Connection = conexion;
-                reader = mycmd.ExecuteReader();
-                conexion.Close();
+                string sql = "INSERT INTO [dbo].[InformacionMedica] ([NSS],[Nombre],[Edad],[TipoSangre],[Enfermedad],[Medicamentos]) VALUES (@NSS, @Nombre, @Edad, @TipoSangre, @Enfermedad, @Medicamentos)";
+                using (SqlConnection conexion = new SqlConnection(Get_ConectionString()))
+                {
+                    using (SqlCommand mycmd = new SqlCommand(sql, conexion))
+                    {
+                        mycmd.Parameters.AddWithValue("@NSS", txtNSS.Text);
+                        mycmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        mycmd.Parameters.AddWithValue("@Edad", txtEdad.Text);
+                        mycmd.Parameters.AddWithValue("@TipoSangre", txtTipoSangre.Text);
+                        mycmd.Parameters.AddWithValue("@Enfermedad", txtEnfermedad.Text);
+                        mycmd.Parameters.AddWithValue("@Medicamentos", txtMedicamentos.Text);
+
+                        conexion.Open();
+                        mycmd.ExecuteNonQuery();
+                    }
+                }
                 BorrarTxt();
                 GridViewInfoMedica.DataBind();
                 SqlDataSourceInfoMedica.DataBind();
             }
             catch (Exception ex)
             {
-
+                // Manejo de excepciones
             }
         }
 
@@ -60,26 +66,32 @@ namespace ProyectoEstancia.InformacionMedica
         {
             try
             {
-                string sql;
-                SqlCommand mycmd = new SqlCommand();
-                SqlDataReader reader;
-                SqlConnection conexion = new SqlConnection(Get_ConectionString());
-                conexion.Open();
-                sql = "UPDATE [dbo].[InformacionMedica] SET [NSS] ='" + txtNSS.Text + "' ,[Nombre] ='" + txtNombre.Text + "',[Edad] ='" + txtEdad.Text + "',[TipoSangre] ='" + txtTipoSangre.Text + "' ,[Enfermedad] ='" + txtEnfermedad.Text + "' ,[Medicamentos] ='" + txtMedicamentos.Text + "' WHERE [NSS] = '" + txtNSS.Text + "'";
-                mycmd.CommandText = sql;
-                mycmd.Connection = conexion;
-                reader = mycmd.ExecuteReader();
-                conexion.Close();
+                string sql = "UPDATE [dbo].[InformacionMedica] SET [NSS] = @NSS, [Nombre] = @Nombre, [Edad] = @Edad, [TipoSangre] = @TipoSangre, [Enfermedad] = @Enfermedad, [Medicamentos] = @Medicamentos WHERE [NSS] = @NSS";
+                using (SqlConnection conexion = new SqlConnection(Get_ConectionString()))
+                {
+                    using (SqlCommand mycmd = new SqlCommand(sql, conexion))
+                    {
+                        mycmd.Parameters.AddWithValue("@NSS", txtNSS.Text);
+                        mycmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        mycmd.Parameters.AddWithValue("@Edad", txtEdad.Text);
+                        mycmd.Parameters.AddWithValue("@TipoSangre", txtTipoSangre.Text);
+                        mycmd.Parameters.AddWithValue("@Enfermedad", txtEnfermedad.Text);
+                        mycmd.Parameters.AddWithValue("@Medicamentos", txtMedicamentos.Text);
+
+                        conexion.Open();
+                        mycmd.ExecuteNonQuery();
+                    }
+                }
                 BorrarTxt();
                 GridViewInfoMedica.DataBind();
                 SqlDataSourceInfoMedica.DataBind();
-              
             }
             catch (Exception ex)
             {
-
+                // Manejo de excepciones
             }
         }
+
 
         protected void GridViewInfoMedica_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -96,26 +108,26 @@ namespace ProyectoEstancia.InformacionMedica
         {
             try
             {
-                string sql = "";
-                SqlCommand mycmd = new SqlCommand();
-                SqlDataReader reader;
-                SqlConnection conexion = new SqlConnection(Get_ConectionString());
-                conexion.Open();
-                sql = "DELETE FROM [dbo].[InformacionMedica] WHERE [NSS] = '" + txtNSS.Text + "' ";
-                mycmd.CommandText = sql;
-                mycmd.Connection = conexion;
-                reader = mycmd.ExecuteReader();
-                conexion.Close();
+                string sql = "DELETE FROM [dbo].[InformacionMedica] WHERE [NSS] = @NSS";
+                using (SqlConnection conexion = new SqlConnection(Get_ConectionString()))
+                {
+                    using (SqlCommand mycmd = new SqlCommand(sql, conexion))
+                    {
+                        mycmd.Parameters.AddWithValue("@NSS", txtNSS.Text);
+
+                        conexion.Open();
+                        mycmd.ExecuteNonQuery();
+                    }
+                }
                 BorrarTxt();
                 GridViewInfoMedica.DataBind();
                 SqlDataSourceInfoMedica.DataBind();
-
-
             }
             catch (Exception ex)
             {
                 // Manejo de excepciones
             }
         }
+
     }
 }
